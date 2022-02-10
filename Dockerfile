73c5486c7c01d4386bf7d86f038b7d88ca3bbd43
Jake Mulford <jakemulf@gmail.com>
2022-02-10 10:39:21 -0500

    layout attempt

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,6 @@
-FROM ghcr.io/deephaven/server
+FROM ghcr.io/deephaven/server AS init-server
 COPY app.d /app.d
+
+FROM ghcr.io/deephaven/web AS init-web
+COPY data/layouts /data/layouts
+RUN chown www-data:www-data /data/layouts
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-docker build --tag deephaven-examples/app-mode-init-python .
+docker build --target init-server --tag deephaven-examples/app-mode-init-python .
+docker build --target init-web --tag deephaven-examples/app-mode-init-python-web .
 docker-compose up $1
