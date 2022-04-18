5f53376a70373c0e680a490099091891c4286c1f
Jake Mulford <jakemulf@gmail.com>
2022-04-18 11:24:20 -0400

    new python library for appmode (#6)

--- data/app.d/init.py
+++ data/app.d/init.py
@@ -5,31 +5,32 @@ A very simple example of initializing a Deephaven server through application mod
 
 This script simply makes a table with 5 values in it.
 
-@author Jake Mulford
 @copyright Deephaven Data Labs LLC
 """
-import jpy
 from deephaven import empty_table
+from deephaven.appmode import ApplicationState, get_app_state
+
 from typing import Callable
 
 ApplicationState = jpy.get_type("io.deephaven.appmode.ApplicationState")
 
-#Non ApplicationState examples
+#Non ApplicationState examples. These global variables are available for use within Deephaven
 def hello():
     print("Hello world")
 
 source = empty_table(5)
 result = source.update(["Values = i"])
 
-#ApplicationState examples
+#ApplicationState examples. These variables are scoped to the application mode function, and
+#only become global variables during the app[<variable>] assignment.
 def start(app: ApplicationState):
     table = empty_table(5)
     table = table.update(["Values = i"])
 
-    app.setField("table", table.j_object)
+    app["table"] = table
 
 def initialize(func: Callable[[ApplicationState], None]):
-  app = jpy.get_type("io.deephaven.appmode.ApplicationContext").get()
+  app = get_app_state()
   func(app)
 
 initialize(start)
